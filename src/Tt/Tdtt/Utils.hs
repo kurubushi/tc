37ae7b9c3410087b900fe54558c5bd982d7ef405
kurubushi <krbshi@gmail.com>
2016-07-07 18:07:20 +0900

    add: tdtt utils prototype
    tdttのもろもろ関数とかを書きはじめたい．

---
+++ src/Tt/Tdtt/Utils.hs
@@ -0,0 +1,13 @@
+module Tt.Tdtt.Utils where
+
+import Atom.Types
+import Set.Types (StateSet)
+import qualified Set.Types as S
+import Tt.Tdtt.Types
+import qualified Data.Map as Map
+import Ta.Ata.Types (Ata(..))
+import qualified Ta.Ata.Types as Ata
+import qualified Ta.Ata.Utils as Ata
+import Ta.Nd.Types (Nd(..))
+import qualified Ta.Nd.Types as Nd
+import qualified Ta.Nd.Utils as Nd
