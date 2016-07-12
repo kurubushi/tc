463e565159dbe831013babc1b99acb5174858804
kurubushi <krbshi@gmail.com>
2016-07-13 04:35:46 +0900

    change: into concrete practical data type
    アルファベットと状態の型を，具体的な型とした．

--- src/Atom/Types.hs
+++ src/Atom/Types.hs
@@ -3,15 +3,33 @@
 module Atom.Types where
 
 import qualified Data.Set as S
+import Data.Semigroup
 
-class Ord a => Alphabet a where
-  end :: a
+data Alphabet = Alphabet Int | AlphabetEnd
+  deriving (Eq, Ord, Show)
 
-class Ord a => Q a
-instance (Ord p, Ord q) => Q ((,) p q)
-instance Ord q => Q (S.Set q)
-  
+isEnd :: Alphabet -> Bool
+isEnd AlphabetEnd = True
+isEnd _           = False
 
-data Tree a where
-  Node :: a -> Tree a -> Tree a -> Tree a
-  TEnd :: Tree a
+isNotEnd :: Alphabet -> Bool
+isNotEnd = not . isEnd
+
+
+data Q = Q {
+    qid   :: Int -- count up from 0
+  , qsize :: Int -- qsize = length of Data.Set Q = max + 1
+} deriving (Eq, Ord, Show)
+
+instance Semigroup Q where
+  q1 <> q2 = makeQ newSize newQ
+    where newSize = qsize q1 * qsize q2
+          newQ    = qid q2 * qsize q1 + qid q2
+
+makeQ :: Int -> Int -> Q
+makeQ n q = Q {qid = q, qsize = n} -- n > q
+
+
+data BTree a where
+  BTNode :: a -> BTree a -> BTree a -> BTree a
+  BTEnd :: BTree a
