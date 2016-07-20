694f1a17e49d3aaa329ad1f8345e0e35d0e43a79
kurubushi <krbshi@gmail.com>
2016-07-20 17:45:07 +0900

    test case: add test tdtt that multiply by three.
    三倍するtdttとか，そういうののテストを追加したり．

--- test/typecheck.hs
+++ test/typecheck.hs
@@ -21,6 +21,10 @@ a2 = makeAlphabet 2
 q0 = makeQ 0 :: QInt
 q1 = makeQ 1 :: QInt
 q2 = makeQ 2 :: QInt
+q3 = makeQ 3 :: QInt
+q4 = makeQ 4 :: QInt
+q5 = makeQ 5 :: QInt
+qe = makeQ 100 :: QInt
 
 tdtt_01_inA = S.fromList [a0, a1]
 tdtt_01_outA = S.fromList [a0, a1, a2]
@@ -77,3 +81,110 @@ nd_01_out2 = Nd.Nd {
       ,((q0,a0), S.fromList [(Nd.Expr (q0,q0))])
       ,((q1,a0), S.fromList [(Nd.Expr (q0,q1)), (Nd.Expr (q1,q0)), (Nd.Expr (q1,q1))])]
 }
+
+-- let inferAta = Tdtt.infer tdtt_01_inA tdtt_01_outA tdtt_01 nd_01_out2 :: Ata.Ata (QD (QInt,QInt)) Set
+-- let inferNd = Ata.toNd tdtt_01_inA inferAta :: Nd.Nd (QD (Set (QD (QInt,QInt)))) Set
+-- let testNd = nd_01_in `Nd.intersection` inferNd :: Nd.Nd (QD (QInt, QD (Set (QD (QInt,QInt))))) Set
+-- let (memo, qs) = Nd.isEmptyWithQneFollow testNd (Nd.getFs testNd) Map.empty
+-- Nd.unsafeSampleCounterExample testNd memo (head . S.toList $ qs)
+
+
+
+z = makeAlphabet 0
+s = makeAlphabet 1
+
+tdtt_02_inA = S.fromList [z, s]
+tdtt_02_outA = S.fromList [z, s]
+
+-- 3倍するtdtt
+tdtt_02 :: Tdtt QInt Set
+tdtt_02 = Tdtt.Tdtt {
+    Tdtt.getPs = S.fromList [q0]
+  , Tdtt.getP0 = S.fromList [q0]
+  , Tdtt.getTrans = tdtt_02_t
+}
+tdtt_02_t :: Tdtt.Trans QInt Set
+tdtt_02_t = Map.fromList
+  [((q0,z), S.fromList [Tdtt.ExprA z (Tdtt.ExprL,Tdtt.ExprL)])
+  ,((q0,s), S.fromList [Tdtt.ExprA s ((Tdtt.ExprA s ((Tdtt.ExprA s (Tdtt.ExprP q0 1,Tdtt.ExprL)),Tdtt.ExprL)),Tdtt.ExprL)])
+  ,((q0,ae), S.fromList [Tdtt.ExprL])] -- わすれないこと！！
+
+-- 2倍
+nd_x2 :: Nd.Nd QInt Set
+nd_x2 = Nd.Nd {
+    Nd.getQs = S.fromList [q0,q1,qe]
+  , Nd.getIs = S.fromList [q0]
+  , Nd.getFs = S.fromList [qe]
+  , Nd.getTrans = Map.fromList
+      [((q0,z), S.fromList [Nd.Expr (qe,qe)])
+      ,((q0,s), S.fromList [Nd.Expr (q1,qe)])
+      ,((q1,s), S.fromList [Nd.Expr (q0,qe)])]
+}
+
+-- 5倍
+nd_x5 :: Nd.Nd QInt Set
+nd_x5 = Nd.Nd {
+    Nd.getQs = S.fromList [q0,q1,q2,q3,q4,qe]
+  , Nd.getIs = S.fromList [q0]
+  , Nd.getFs = S.fromList [qe]
+  , Nd.getTrans = Map.fromList
+      [((q0,z), S.fromList [Nd.Expr (qe,qe)])
+      ,((q0,s), S.fromList [Nd.Expr (q4,qe)])
+      ,((q1,s), S.fromList [Nd.Expr (q0,qe)])
+      ,((q2,s), S.fromList [Nd.Expr (q1,qe)])
+      ,((q3,s), S.fromList [Nd.Expr (q2,qe)])
+      ,((q4,s), S.fromList [Nd.Expr (q3,qe)])]
+}
+-- 5倍でない
+nd_notx5 :: Nd.Nd QInt Set
+nd_notx5 = Nd.Nd {
+    Nd.getQs = S.fromList [q0,q1,q2,q3,q4,qe]
+  , Nd.getIs = S.fromList [q1,q2,q3,q4]
+  , Nd.getFs = S.fromList [qe]
+  , Nd.getTrans = Map.fromList
+      [((q0,z), S.fromList [Nd.Expr (qe,qe)])
+      ,((q0,s), S.fromList [Nd.Expr (q4,qe)])
+      ,((q1,s), S.fromList [Nd.Expr (q0,qe)])
+      ,((q2,s), S.fromList [Nd.Expr (q1,qe)])
+      ,((q3,s), S.fromList [Nd.Expr (q2,qe)])
+      ,((q4,s), S.fromList [Nd.Expr (q3,qe)])]
+}
+
+-- 6倍
+nd_x6 :: Nd.Nd QInt Set
+nd_x6 = Nd.Nd {
+    Nd.getQs = S.fromList [q0,q1,q2,q3,q4,q5,qe]
+  , Nd.getIs = S.fromList [q0]
+  , Nd.getFs = S.fromList [qe]
+  , Nd.getTrans = Map.fromList
+      [((q0,z), S.fromList [Nd.Expr (qe,qe)])
+      ,((q0,s), S.fromList [Nd.Expr (q5,qe)])
+      ,((q1,s), S.fromList [Nd.Expr (q0,qe)])
+      ,((q2,s), S.fromList [Nd.Expr (q1,qe)])
+      ,((q3,s), S.fromList [Nd.Expr (q2,qe)])
+      ,((q4,s), S.fromList [Nd.Expr (q3,qe)])
+      ,((q5,s), S.fromList [Nd.Expr (q4,qe)])]
+}
+-- 6倍でない
+nd_notx6 :: Nd.Nd QInt Set
+nd_notx6 = Nd.Nd {
+    Nd.getQs = S.fromList [q0,q1,q2,q3,q4,q5,qe]
+  , Nd.getIs = S.fromList [q1,q2,q3,q4,q5]
+  , Nd.getFs = S.fromList [qe]
+  , Nd.getTrans = Map.fromList
+      [((q0,z), S.fromList [Nd.Expr (qe,qe)])
+      ,((q0,s), S.fromList [Nd.Expr (q5,qe)])
+      ,((q1,s), S.fromList [Nd.Expr (q0,qe)])
+      ,((q2,s), S.fromList [Nd.Expr (q1,qe)])
+      ,((q3,s), S.fromList [Nd.Expr (q2,qe)])
+      ,((q4,s), S.fromList [Nd.Expr (q3,qe)])
+      ,((q5,s), S.fromList [Nd.Expr (q4,qe)])]
+}
+
+-- Tdtt.typecheck tdtt_02_inA tdtt_02_outA nd_02_in nd_x6 tdtt_02
+-- Tdtt.typecheck tdtt_02_inA tdtt_02_outA nd_02_in nd_x5 tdtt_02
+-- let inferAta = Tdtt.infer tdtt_02_inA tdtt_02_outA tdtt_02 nd_x5 :: Ata.Ata (QD (QInt,QInt)) Set
+-- let inferNd = Ata.toNd tdtt_02_inA inferAta :: Nd.Nd (QD (Set (QD (QInt,QInt)))) Set
+-- let testNd = nd_02_in `Nd.intersection` inferNd :: Nd.Nd (QD (QInt, QD (Set (QD (QInt,QInt))))) Set
+-- let (memo, qs) = Nd.isEmptyWithQneFollow testNd (Nd.getFs testNd) Map.empty
+-- Nd.unsafeSampleCounterExample testNd memo (head . S.toList $ qs)
