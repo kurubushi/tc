cb1d8408d51b4115f1d12f0c266a50c4f2f2d4a5
kurubushi <krbshi@gmail.com>
2016-07-23 19:28:53 +0900

    parser: add testing parser...
    これはテスト段階．
    試しながらやっていくぞ．

---
+++ src/Parser/Parser.hs
@@ -0,0 +1,106 @@
+module Parser where
+
+import Text.Parsec
+import Text.ParserCombinators.Parsec.Combinator
+import Text.Parsec.String
+import Data.Set (Set)
+import qualified Data.Set as Set
+import qualified Data.Map.Lazy as Map
+import Atom.Types
+import qualified Ta.Nd.Types as Nd
+import qualified Ta.Nd.Utils as Nd
+import qualified Tt.Tdtt.Types as Tdtt
+import qualified Tt.Tdtt.Utils as Tdtt
+import Control.Monad
+import Control.Applicative ((<$>), (*>), (<*), (<*>))
+
+parseToSetAs :: Ord a => Parser a -> String -> Parser (Set a)
+parseToSetAs p st = Set.fromList
+  <$> (spaces *> string st
+       *> spaces *> char '='
+       *> spaces *> char '{'
+       *> spaces *> sepBy (p <* spaces) (colon *> spaces)
+       <* spaces <* char '}')
+
+var :: Parser String
+var = (:) <$> lower <*> many alphaNum
+
+-- NdRules = {q0 -> a(q1,q2), q2 -> a(q2,q2)}
+ndRules :: Parser (Nd.Trans (QD String) Set)
+ndRules = mkTransFromSet <$> parseToSetAs ndRule "NdRules"
+  where
+  mkTransFromSet = Set.foldr combine Map.empty
+  combine = Map.unionWith Set.union
+
+-- q -> a (q1, q2)
+ndRule :: Parser (Nd.Trans (QD String) Set)
+ndRule = makeRule
+  <$> (spaces *> var) -- q
+  <*> (spaces *> string "->" *> spaces *> var) -- a
+  <*> (spaces *> char '(' *> spaces *> var) -- q1
+  <*> (spaces *> colon *> spaces *> var) -- q2
+  <*  (spaces *> char ')')
+  <*  spaces
+  where
+  makeRule q a q1 q2 = Map.singleton
+    (makeQ q, makeAlphabet a) (Set.singleton (Nd.Expr (makeQ q1, makeQ q2)))
+
+-- TdttRules = {tdttrule, tdttrule}
+tdttRules :: Parser (Tdtt.Trans (QD String) Set)
+tdttRules = mkTransFromSet <$> parseToSetAs tdttRule "TdttRules"
+  where
+  mkTransFromSet = Set.foldr combine Map.empty
+  combine = Map.unionWith Set.union
+
+-- p(a) -> b(p1(1),c(p2(2),#)) === p(a(x1,x2) -> b(p1(x1),c(p2(x2),#))
+tdttRule :: Parser (Tdtt.Trans (QD String) Set)
+tdttRule = makeRule
+  <$> (spaces *> var) -- p
+  <*> (spaces *> char '(' *> spaces *> var) -- a
+  <*> (spaces *> string "->" *> spaces *> tdttExpr) -- expr
+  where
+  makeRule p a expr = Map.singleton
+    (makeQ p, makeAlphabet a) (Set.singleton expr)
+
+tdttExpr :: Parser (Tdtt.Expr (QD String))
+tdttExpr = choice . map try $ [tdttExprA, tdttExprL, tdttExprP]
+  -- it must use try
+
+tdttExprA :: Parser (Tdtt.Expr (QD String))
+tdttExprA = makeExpr
+  <$> (spaces *> var) -- a
+  <*> (spaces *> char '(' *> spaces *> tdttExpr) -- e1
+  <*> (spaces *> colon *> spaces *> tdttExpr) -- e2
+  <*  (spaces *> char ')')
+  <*  spaces
+  where
+  makeExpr a e1 e2 = Tdtt.ExprA (makeAlphabet a) (e1,e2)
+
+tdttExprL :: Parser (Tdtt.Expr (QD String))
+tdttExprL = spaces *> endAlphabetSt *> spaces *> pure Tdtt.ExprL
+
+tdttExprP :: Parser (Tdtt.Expr (QD String))
+tdttExprP = makeExpr
+  <$> (spaces *> var) -- p
+  <*> (spaces *> char '(' *> spaces *> number) -- 1
+  <*  (spaces *> char ')')
+  where
+  makeExpr p = Tdtt.ExprP (makeQ p)
+
+colon :: Parser Char
+colon = char ','
+
+number :: Parser Int
+number = read <$> many1 digit
+
+endAlphabetSt :: Parser String
+endAlphabetSt = string "#"
+
+
+-- States = {q0, q1, q2}
+states :: Parser (Set (QD String))
+states = Set.map makeQ <$> parseToSetAs var "States"
+
+-- Alphabets = {a, b}
+alphabets :: Parser (Set Alphabet)
+alphabets = Set.map makeAlphabet <$> parseToSetAs var "Alphabets"
