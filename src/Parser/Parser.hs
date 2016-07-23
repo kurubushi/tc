9d0d28dbc9b396416ee71ef29da960740c807409
kurubushi <krbshi@gmail.com>
2016-07-23 23:07:54 +0900

    parse: add Nd and Tdtt parser
    NdとTdttをパースするやつを作成．
    ちょっと考えていた，状態を変数でとかいうのは後回しにすることにした．
    いったん，動くものがほしい．

--- src/Parser/Parser.hs
+++ src/Parser/Parser.hs
@@ -5,6 +5,7 @@ import Text.ParserCombinators.Parsec.Combinator
 import Text.Parsec.String
 import Data.Set (Set)
 import qualified Data.Set as Set
+import Data.Map.Lazy (Map)
 import qualified Data.Map.Lazy as Map
 import Atom.Types
 import qualified Ta.Nd.Types as Nd
@@ -15,6 +16,13 @@ import Control.Monad
 import Control.Applicative ((<$>), (*>), (<*), (<*>))
 
 type Var = String
+
+type EMap = Map Var Expr
+
+data Expr = EAlphabet (Set Alphabet)
+          | ENd (Nd.Trans (QD String) Set)
+          | ETdtt (Tdtt.Trans (QD String) Set)
+
 
 parseAs :: String -> Parser a -> Parser a
 parseAs tag p = spaces *> string tag *> spaces *> p
@@ -35,6 +43,16 @@ parseToSetAs tag = parseAs tag . parseToSet
 
 var :: Parser Var
 var = (:) <$> lower <*> many alphaNum
+
+-- Nd {states, states, states, rules}
+-- let (Right myNd) = parse nd "" "Nd (States {q0,q1,q2}, States{q2}, States {q0}, NdRules {q2-> a (q0,q0), q2 -> b(q0,q0)})"
+nd :: Parser (Nd.Nd (QD String) Set)
+nd = parseAs "Nd" $ Nd.Nd
+  <$> (spaces *> char '(' *> spaces *> states) -- Q
+  <*> (spaces *> colon *> spaces *> states) -- I
+  <*> (spaces *> colon *> spaces *> states) -- F
+  <*> (spaces *> colon *> spaces *> ndRules) -- Delta
+  <*  spaces <* char ')' <* spaces
 
 -- "ndr = NdRules {q0 -> a(q1,q2), q2 -> a(q2,q2)}"
 ndRulesVar :: Parser (Var, Nd.Trans (QD String) Set)
@@ -59,6 +77,16 @@ ndRule = makeRule
   where
   makeRule q a q1 q2 = Map.singleton
     (makeQ q, makeAlphabet a) (Set.singleton (Nd.Expr (makeQ q1, makeQ q2)))
+
+
+-- Tdtt {states, states, rules}
+-- let (Right myTdtt) = parse tdtt "" "Tdtt (States {fib, aux}, States{fib}, TdttRules {fib(#) -> #, fib(a) -> a(fib(1),aux(1)), aux(#) -> #, aux(a) -> fib(1)})"
+tdtt :: Parser (Tdtt.Tdtt (QD String) Set)
+tdtt = parseAs "Tdtt" $ Tdtt.Tdtt
+  <$> (spaces *> char '(' *> spaces *> states) -- P
+  <*> (spaces *> colon *> spaces *> states) -- P0
+  <*> (spaces *> colon *> spaces *> tdttRules) -- Pi
+  <*  spaces <* char ')' <* spaces
 
 -- "tdttr = TdttRules {p(a) -> b(p1(1),c(p2(2),#)), p(#) -> #}"
 tdttRulesVar :: Parser (Var, Tdtt.Trans (QD String) Set)
