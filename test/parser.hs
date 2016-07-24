37e83d7b5142f1d736dc1ce616d582261784c501
kurubushi <krbshi@gmail.com>
2016-07-24 20:34:43 +0900

    add: print testNd size to test program
    テストプログラム，testNdのサイズを表示するようにしてみた．
    この変更による速度の低下は見られなかった.

--- test/parser.hs
+++ test/parser.hs
@@ -5,6 +5,7 @@ import System.Environment
 import System.IO
 import Atom.Types
 import Control.Monad
+import System.IO.Unsafe
 import qualified Parser.Parser as P
 import Ta.Ata.Types (Ata(..))
 import qualified Ta.Ata.Types as Ata
@@ -33,29 +34,32 @@ exec :: P.EMap -> P.Exec -> IO ()
 exec m ch@(P.Typecheck iav oav indv ondv tdttv) = do
   putStr "Start!: "
   print ch
+  putStrLn $ "\ttestNd stateset size: " ++ show size
   (maybe
     (putStrLn "True")
     (\t -> putStrLn "False. A counter example is,"
       >> putStrLn (showBTree t))
-    (testTypeCheck ia oa ind ond tdtt))
+    mt)
   where
   (P.EAlphabet ia) = m Map.! iav
   (P.EAlphabet oa) = m Map.! oav
   (P.ENd ind) = m Map.! indv
   (P.ENd ond) = m Map.! ondv
   (P.ETdtt tdtt) = m Map.! tdttv
+  (size, mt) = testTypeCheck ia oa ind ond tdtt
 
 testTypeCheck :: forall p q1 q2 q3 q4 q5.
   (Q p, Q q1, Q q2,
    q3 ~ QD (p,q2),
    q4 ~ QD (Set q3),
    q5 ~ QD (q1,q4)) =>
-   Set Alphabet -> Set Alphabet -> Nd q1 Set -> Nd q2 Set -> Tdtt p Set -> Maybe (BTree Alphabet)
+   Set Alphabet -> Set Alphabet -> Nd q1 Set -> Nd q2 Set -> Tdtt p Set -> (Int, Maybe (BTree Alphabet))
 testTypeCheck inputAs outputAs inputNd outputNd tdtt
-  | S.null qs = Nothing
-  | otherwise = Nd.sampleCounterExample testNd memo (head . S.toList $ qs)
+  | S.null qs = (testNdSize, Nothing)
+  | otherwise = (testNdSize, Nd.sampleCounterExample testNd memo (head . S.toList $ qs))
   where
   inferAta = Tdtt.infer inputAs outputAs tdtt outputNd :: Ata q3 Set
   inferNd = Ata.toNd inputAs inferAta :: Nd q4 Set
   testNd = inputNd `Nd.intersection` inferNd :: Nd q5 Set
+  testNdSize = S.size $ Nd.getQs testNd
   (memo, qs) = Nd.isEmptyWithQneFollow testNd (Nd.getFs testNd) Map.empty
